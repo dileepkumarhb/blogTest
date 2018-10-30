51d60e75812d666eb5d5e0dfbd8772bc018784ef
dileep <dileepbannikatti@gmail.com>
2018-10-30 13:54:57 +0530

    configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,14 +9,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+//Vue.js
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import Buefy from 'buefy';
+Vue.use(Buefy)
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
